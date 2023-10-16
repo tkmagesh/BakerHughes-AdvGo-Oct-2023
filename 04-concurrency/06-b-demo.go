12aba15cd24b5d249645764f37523b412da63853
Magesh Kuppan <tkmagesh77@gmail.com>
2023-10-16 17:25:06 +0530

    Day-01 complete

--- 04-concurrency/06-b-demo.go
+++ 04-concurrency/06-b-demo.go
@@ -3,23 +3,19 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func main() {
-	wg := &sync.WaitGroup{}
 
 	// share memory by communicating
 	ch := make(chan int)
-	wg.Add(1)
-	go add(100, 20, wg, ch)
-	wg.Wait()
+
+	go add(100, 20, ch)
 	result := <-ch //receiving result from the channel
 	fmt.Println(result)
 }
 
-func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+func add(x, y int, ch chan int) {
 	result := x + y
 	ch <- result //sending result to the channel
-	wg.Done()
 }
